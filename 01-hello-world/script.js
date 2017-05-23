dbf5c274a941b254b70efacd0dea0185a04f91a4
Pawel Mendelski <pawel.mendelski@allegrogroup.com>
2017-05-23 19:02:51 +0200

    01-hello-world: Create element using browser DOM

--- 01-hello-world/script.js
+++ 01-hello-world/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+function createElement(type, text) {
+  var element = document.createElement(type);
+  element.innerText = text;
+  return element;
+}
+
+document
+  .getElementById('root')
+  .appendChild(createElement('h1', 'Hello from DOM'));
