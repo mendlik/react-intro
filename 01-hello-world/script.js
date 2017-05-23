f4be5bf717dd012d3fa1cf6eca2192baea88e4d0
Pawel Mendelski <pawel.mendelski@allegrogroup.com>
2017-05-23 19:04:43 +0200

    01-hello-world: Create element using React

--- 01-hello-world/script.js
+++ 01-hello-world/script.js
@@ -1,9 +1,4 @@
-function createElement(type, text) {
-  var element = document.createElement(type);
-  element.innerText = text;
-  return element;
-}
-
-document
-  .getElementById('root')
-  .appendChild(createElement('h1', 'Hello from DOM'));
+ReactDOM.render(
+  <h1>Hello from React</h1>,
+  document.getElementById('root')
+);
